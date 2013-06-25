8fe657ebc2f4ad0bed847f811fd9b4266fbaa0a1
Lucas BIGUET-MERMET <lucas.biguetmermet@gmail.com>
2013-06-25 18:35:02 +0200

    initial commit: base site done

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+	def home
+		
+	end
+	def tour
+		
+	end
+	def pro
+		
+	end
+	def contact
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wailingtrees::Application.config.secret_token = 'e0077cc9ca2e69fe00cdbfb064e97b2f3f6209a173f148c3c17287c593bdc83eeb1202e1e46535c398fc48cc4af579897167f6765a57bb154e7a07b8ac1133d0'
